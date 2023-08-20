de02371446e6cbc059e3557a0f443dec822c54bd
roopesh <lcb2022032@iiitl.ac.in>
2023-08-20 20:06:40 +0530

    Test.js for the3 solidity contract has been updated

--- Blockchain-new/test/TokenGame.js
+++ Blockchain-new/test/TokenGame.js
@@ -4,12 +4,11 @@ describe("TokenGame", function () {
   let TokenGame;
   let tokenGame;
   let owner;
-  let addr1;
+  let player1;
+  let player2;
 
   beforeEach(async function () {
-    const signers = await ethers.getSigners();
-    owner = signers[0];
-    addr1 = signers[1];
+    [owner, player1, player2] = await ethers.getSigners();
 
     TokenGame = await ethers.getContractFactory("TokenGame");
     tokenGame = await TokenGame.deploy();
@@ -20,54 +19,181 @@ describe("TokenGame", function () {
     expect(await tokenGame.owner()).to.equal(owner.address);
   });
 
-  it("Should create a task", async function () {
-    await tokenGame.connect(addr1).createTask(100);
-    const task = await tokenGame.getTask(0);
-    expect(task.creator).to.equal(addr1.address);
-    expect(task.reward).to.equal(100);
-    expect(task.completed).to.equal(false);
+  it("Should buy tokens ", async function () {
+    const ethAmount = ethers.utils.parseEther("0.5");
+    const initialPlayer1Balance = await tokenGame.getBalance(await tokenGame.getPlayerIdHash(player1.address));
+
+    await expect(async () => {
+        await tokenGame.connect(player1).buyTokens({ value: ethAmount });
+    }).to.changeEtherBalances([player1], [ethAmount.mul(-1)]);
+
+    const finalPlayer1Balance = await tokenGame.getBalance(await tokenGame.getPlayerIdHash(player1.address));
+    expect(finalPlayer1Balance).to.equal(initialPlayer1Balance.add(1));
+
+    const player1Tokens = await tokenGame.getBalance(await tokenGame.getPlayerIdHash(player1.address));
+    expect(player1Tokens).to.equal(2);
+
+    const player2EthAmount = ethers.utils.parseEther("0.7");
+    const initialPlayer2Balance = await player2.getBalance();
+
+    await expect(async () => {
+        await tokenGame.connect(player2).buyTokens({ value: player2EthAmount });
+    }).to.changeEtherBalances([player2], [player2EthAmount.mul(-1)]);
+
+    const finalPlayer2Balance = await player2.getBalance();
+    expect(finalPlayer2Balance).to.equal(initialPlayer2Balance.sub(player2EthAmount));
+
+    const player2Tokens = await tokenGame.getBalance(await tokenGame.getPlayerIdHash(player2.address));
+    expect(player2Tokens).to.equal(2);
+
+    const initialOwnerBalance = await player1.getBalance();
+
+    await expect(async () => {
+        await tokenGame.connect(player1).buyTokens({ value: ethAmount });
+    }).to.changeEtherBalances([player1], [ethAmount.mul(-1)]);
+
+    const finalOwnerBalance = await player1.getBalance();
+    expect(finalOwnerBalance).to.equal(initialOwnerBalance.sub(ethAmount));
+});
+
+  it("Should send tokens", async function () {
+    const initialBalance = 10;
+    await tokenGame.connect(player1).initializePlayer(ethers.utils.formatBytes32String("player1"));
+    await tokenGame.connect(player2).initializePlayer(ethers.utils.formatBytes32String("player2"));
+
+    await tokenGame.connect(player1).buyTokens(initialBalance);
+    const initialPlayer1Balance = await tokenGame.getBalance(await tokenGame.getPlayerIdHash(player1.address));
+    const initialPlayer2Balance = await tokenGame.getBalance(await tokenGame.getPlayerIdHash(player2.address));
+
+    const sendAmount = 3;
+    await tokenGame.connect(player1).sendTokens(await tokenGame.getPlayerIdHash(player2.address), sendAmount);
+
+    const finalPlayer1Balance = await tokenGame.getBalance(await tokenGame.getPlayerIdHash(player1.address));
+    const finalPlayer2Balance = await tokenGame.getBalance(await tokenGame.getPlayerIdHash(player2.address));
+
+    expect(finalPlayer1Balance).to.equal(initialPlayer1Balance - sendAmount);
+    expect(finalPlayer2Balance).to.equal(initialPlayer2Balance + sendAmount);
   });
 
-  it("Should complete a task and mint a token", async function () {
-    await tokenGame.connect(addr1).createTask(100);
-    await tokenGame.connect(addr2).completeTask(0);
-    const task = await tokenGame.getTask(0);
-    expect(task.completed).to.equal(true);
+  it("Should stake and unstake tokens", async function () {
+    const initialBalance = 10;
+    await tokenGame.connect(player1).initializePlayer(ethers.utils.formatBytes32String("player1"));
 
-    const ownerBalanceBefore = await tokenGame.balanceOf(owner.address);
-    await tokenGame.connect(addr2).claimTokens();
-    const ownerBalanceAfter = await tokenGame.balanceOf(owner.address);
+    await tokenGame.connect(player1).buyTokens(initialBalance);
+    const initialPlayer1Balance = await tokenGame.getBalance(await tokenGame.getPlayerIdHash(player1.address));
 
-    expect(ownerBalanceAfter).to.equal(ownerBalanceBefore.add(1));
+    const stakedAmount = 5;
+    await tokenGame.connect(player1).stakeTokens(stakedAmount);
+
+    const stakedPlayer1Balance = await tokenGame.getStakedBalance(await tokenGame.getPlayerIdHash(player1.address));
+    expect(stakedPlayer1Balance).to.equal(stakedAmount);
+
+    await tokenGame.connect(player1).unstakeTokens(stakedAmount);
+
+    const finalPlayer1Balance = await tokenGame.getBalance(await tokenGame.getPlayerIdHash(player1.address));
+    expect(finalPlayer1Balance).to.equal(initialPlayer1Balance);
+  });
+  it("Should create and complete challenges", async function () {
+    const description = "Complete the mission!";
+    const reward = 10;
+
+    await tokenGame.connect(player1).createChallenge(description, reward);
+    const challenge = await tokenGame.challenges(player1.address);
+    expect(challenge.description).to.equal(description);
+    expect(challenge.reward).to.equal(reward);
+    expect(challenge.active).to.equal(true);
+
+    await tokenGame.connect(player1).completeChallenge();
+    const player1Balance = await tokenGame.getBalance(await tokenGame.getPlayerIdHash(player1.address));
+    expect(player1Balance).to.equal(initialBalance + reward);
+    const playerPower = await tokenGame.getPower(await tokenGame.getPlayerIdHash(player1.address));
+    expect(playerPower).to.equal(10);
+
+    const completedChallengeCount = await tokenGame.completedChallengesCount(player1.address);
+    expect(completedChallengeCount).to.equal(1);
   });
 
-  it("Should buy tokens", async function () {
+  it("Should upgrade character abilities", async function () {
+    const initialPower = 20;
+    await tokenGame.connect(player1).initializePlayer(ethers.utils.formatBytes32String("player1"));
+    await tokenGame.connect(player1).buyTokens(initialPower);
+
+    const upgradeAmount = 5;
+    await tokenGame.connect(player1).upgradeCharacterAbilities(upgradeAmount);
+
+    const player1Power = await tokenGame.getPower(await tokenGame.getPlayerIdHash(player1.address));
+    expect(player1Power).to.equal(initialPower + upgradeAmount);
+  });
+
+  it("Should complete missions", async function () {
+    const initialPower = 5;
+    await tokenGame.connect(player1).initializePlayer(ethers.utils.formatBytes32String("player1"));
+    await tokenGame.connect(player1).buyTokens(initialPower);
+
+    await tokenGame.connect(player1).increasePower(5);
+
+    const description = "Win 3 games!";
+    const targetWinCount = 3;
+    await tokenGame.connect(player1).createMission(description, targetWinCount);
+
+    await tokenGame.connect(player1).completeMission(description);
+    const player1Balance = await tokenGame.getBalance(await tokenGame.getPlayerIdHash(player1.address));
+    expect(player1Balance).to.equal(initialBalance + missionReward);
+  });
+
+  it("Should spin the wheel and win rewards", async function () {
     const ethAmount = ethers.utils.parseEther("0.1");
-    const buyerBalanceBefore = await owner.getBalance();
+    await tokenGame.connect(player1).buyTokens({ value: ethAmount });
 
-    await expect(() =>
-      tokenGame.connect(addr1).buyTokens({ value: ethAmount })
-    ).to.changeEtherBalance(owner, ethAmount.neg());
+    const player1InitialBalance = await tokenGame.getBalance(await tokenGame.getPlayerIdHash(player1.address));
 
-    const buyerBalanceAfter = await owner.getBalance();
-    expect(buyerBalanceAfter).to.equal(buyerBalanceBefore.sub(ethAmount));
+    await tokenGame.connect(player1).spinWheel();
 
-    const buyerTokens = await tokenGame.balanceOf(owner.address);
-    expect(buyerTokens).to.equal(1);
+    const player1FinalBalance = await tokenGame.getBalance(await tokenGame.getPlayerIdHash(player1.address));
+    expect(player1FinalBalance).to.be.above(player1InitialBalance);
   });
 
-  it("Should prevent multiple claims", async function () {
-    await tokenGame.connect(addr1).claimTokens();
-    await expect(tokenGame.connect(addr1).claimTokens()).to.be.revertedWith(
-      "Tokens already claimed"
-    );
-  });
+  it("Should play Rock-Paper-Scissors ", async function () {
+    await tokenGame.connect(player1).buyTokens(10);
+    await tokenGame.connect(player2).buyTokens(10);
 
-  it("Should prevent multiple completions of the same task", async function () {
-    await tokenGame.connect(addr1).createTask(100);
-    await tokenGame.connect(addr2).completeTask(0);
-    await expect(tokenGame.connect(addr2).completeTask(0)).to.be.revertedWith(
-      "Task already completed"
-    );
-  });
+    await tokenGame.connect(player1).submitRPSChoice(0);
+    await tokenGame.connect(player2).submitRPSChoice(2);
+
+    const player1BalanceBefore = await tokenGame.getBalance(await tokenGame.getPlayerIdHash(player1.address));
+    const player2BalanceBefore = await tokenGame.getBalance(await tokenGame.getPlayerIdHash(player2.address));
+
+    await tokenGame.connect(player1).revealRPSChoice(0);
+    await tokenGame.connect(player2).revealRPSChoice(2);
+
+    const player1BalanceAfter = await tokenGame.getBalance(await tokenGame.getPlayerIdHash(player1.address));
+    const player2BalanceAfter = await tokenGame.getBalance(await tokenGame.getPlayerIdHash(player2.address));
+
+    expect(player1BalanceAfter).to.equal(player1BalanceBefore + 2);
+    expect(player2BalanceAfter).to.equal(player2BalanceBefore - 2);
+
+    await tokenGame.connect(player1).submitRPSChoice(1);
+    await tokenGame.connect(player2).submitRPSChoice(1);
+
+    const player1BalanceBeforeTie = await tokenGame.getBalance(await tokenGame.getPlayerIdHash(player1.address));
+    const player2BalanceBeforeTie = await tokenGame.getBalance(await tokenGame.getPlayerIdHash(player2.address));
+
+    await tokenGame.connect(player1).revealRPSChoice(1);
+    await tokenGame.connect(player2).revealRPSChoice(1);
+
+    const player1BalanceAfterTie = await tokenGame.getBalance(await tokenGame.getPlayerIdHash(player1.address));
+    const player2BalanceAfterTie = await tokenGame.getBalance(await tokenGame.getPlayerIdHash(player2.address));
+
+    expect(player1BalanceAfterTie).to.equal(player1BalanceBeforeTie);
+    expect(player2BalanceAfterTie).to.equal(player2BalanceBeforeTie);
+
+    await expect(tokenGame.connect(player1).submitRPSChoice(3)).to.be.revertedWith("Invalid choice");
+    await expect(tokenGame.connect(player2).submitRPSChoice(3)).to.be.revertedWith("Invalid choice");
+
+    await tokenGame.connect(player1).revealRPSChoice(0);
+
+    await expect(tokenGame.connect(player1).revealRPSChoice(0)).to.be.revertedWith("Choice already revealed");
+
+    await expect(tokenGame.connect(player2).revealRPSChoice(1)).to.be.revertedWith("Choice not submitted");
+});
 });
